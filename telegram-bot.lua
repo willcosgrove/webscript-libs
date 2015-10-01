fe46407fd4119f72eab30cb0be50ce0a5bc9f9c1
Will Cosgrove <will@willcosgrove.com>
2015-10-01 17:48:58 -0500

    Add constructor to telegram-bot

--- telegram-bot.lua
+++ telegram-bot.lua
@@ -1,5 +1,12 @@
 TelegramBot = {}
 
+function TelegramBot:new(o)
+  o = o or {}
+  setmetatable(o, self)
+  self.__index = self
+  return o
+end
+
 function TelegramBot:run(request)
   payload = json.parse(request.body)
   if string.match(payload.message.text, "^/%a+") then
