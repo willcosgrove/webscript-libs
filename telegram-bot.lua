150d6b312ab3d2f53392adb706e9aadbebe158c7
Will Cosgrove <will@willcosgrove.com>
2015-10-01 18:09:43 -0500

    Ignore bot username

--- telegram-bot.lua
+++ telegram-bot.lua
@@ -10,7 +10,7 @@ end
 function TelegramBot:run(request)
   payload = json.parse(request.body)
   if string.match(payload.message.text, "^/%a+") then
-    _, _, command, args = string.find(payload.message.text, "/(%a+)%s?(.*)")
+    _, _, command, args = string.find(payload.message.text, "/(%a+)@?%a+%s?(.*)")
     return self[command](args, payload)
   else
     return 200
